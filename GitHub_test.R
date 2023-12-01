beea01075bde3492f356d0a495e4852b2f86b6de
Joe Beesley <joseph.beesley@forestresearch.gov.uk>
2023-12-01 11:52:34 +0000

    Staged

--- GitHub_test.R
+++ GitHub_test.R
@@ -1,8 +1,5 @@
 library(tidyverse)
 library(terra)
-library(usethis)
-
-use_git()
 
 japan = vect("JPN_adm1.shp")
 df.japan = read.csv("Jpn_liveBirth-perf2.csv")
@@ -14,5 +11,4 @@ japan = terra::merge(japan, df.japan)
 
 plot(japan,
      "Population_2011",
-     col = colorRampPalette(brewer.pal(9, "YlGnBu"))(18),
      main = "Japanese population density by CC.AA. (2011)")
