f4d1c7d7ca6fe3d43757ac536bf73f85e77a92c6
Joe Beesley <joseph.beesley@forestresearch.gov.uk>
2023-12-01 11:47:21 +0000

    Added population density data

--- GitHub_test.R
+++ GitHub_test.R
@@ -5,5 +5,14 @@ library(usethis)
 use_git()
 
 japan = vect("JPN_adm1.shp")
+df.japan = read.csv("Jpn_liveBirth-perf2.csv")
 
-plot(japan, "NAME_1")
+colnames(df.japan)[1] = "NAME_1"
+colnames(df.japan)[2] = "Population_2011"
+
+japan = terra::merge(japan, df.japan)
+
+plot(japan,
+     "Population_2011",
+     col = colorRampPalette(brewer.pal(9, "YlGnBu"))(18),
+     main = "Japanese population density by CC.AA. (2011)")
