047cf6c0ca7602387a3d8debab479a1f0d2dbcc3
Joe Beesley <joseph.beesley@forestresearch.gov.uk>
2023-12-01 12:40:14 +0000

    Tidy

--- GitHub_test.R
+++ GitHub_test.R
@@ -7,12 +7,11 @@ japan = vect("JPN_adm1.shp")
 df.japan = read.csv("Jpn_liveBirth-perf2.csv")
 
 colnames(df.japan)[1] = "NAME_1"
-colnames(df.japan)[2] = "Population_2011"
 
 japan = terra::merge(japan, df.japan)
 
 ggplot(japan)+
-  geom_spatvector(aes(fill = Population_2011),
+  geom_spatvector(aes(fill = X_2011),
                   col = 'black') +
   scale_fill_viridis() +
   labs(title = "Japanese population density by CC.AA. (2011)",
